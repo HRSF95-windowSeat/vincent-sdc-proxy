8a713fa3b121b2fe50319f07a183c5fd00361543
Anoop Sownderraj <asownder95@gmail.com>
2018-06-15 19:20:42 -0700

    Finished setup and importing of modules in proxy server.

--- client/src/Webpage.jsx
+++ client/src/Webpage.jsx
@@ -3,45 +3,12 @@ import styles from '../styles/Webpage.css';
 
 const Webpage = props => (
   <div>
-    <nav className={`navbar navbar-expand-lg ${styles.navBorder} ${styles.navbar}`}>
-      <div className={`${styles.navBorder} ${styles.navBrand}`}>
-        <a className={`navbar-brand ${styles['navbar-brand']}`} href="#">{'CavaTable'}</a>
-      </div>
-      <div className={`${styles.navBorder} ${styles.locationSelectorContainer}`}>
-        <span className={`fa fa-map-marker fa-lg ${styles.locationSelectorIcon}`}></span>
-        <span className={styles.locationSelector}>
-          {'San Francisco Bay Area   San Francisco'}
-        </span>
-      </div>
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <div className={styles.signupBtn}>
-            <a className={styles.signupBtn_navlink} href="#">{'Sign Up'}</a>
-          </div>
-        </li>
-        <li className="nav-item">
-          <a className={`nav-link ${styles['nav-link']}`} href="#">{'Sign In'}</a>
-        </li>
-      </ul>
-    </nav>
-
-    <nav aria-label="breadcrumb">
-      <ol className={`breadcrumb ${styles.breadcrumbList}`}>
-        <li className={`breadcrumb-item ${styles.breadcrumbItem}`}><a href="#">{'Home'}</a></li>
-        <li className={`breadcrumb-item ${styles.breadcrumbItem}`}><a href="#">{'United States'}</a></li>
-        <li className={`breadcrumb-item ${styles.breadcrumbItem}`}><a href="#">{'San Francisco Bay Area'}</a></li>
-        <li className={`breadcrumb-item ${styles.breadcrumbItem} active`} aria-current="page">{'San Francisco'}</li>
-      </ol>
-    </nav>
-
-    <div className={`jumbotron ${styles.jumbotron}`}></div>
-
     <div className={`container-fluid ${styles.bodyContainer}`}>
       <div className="row">
         <div className="col-1"></div>
         <div className="col-6">
           <div className="container">
-            <nav className={`navbar ${styles.navbar} ${styles.bodyNav} ml-auto sticky-top`}>
+            <nav id="navbar" className={`navbar ${styles.navbar} ${styles.bodyNav} ml-auto sticky-top`}>
               <ul className={`navbar-nav navbar-expand mr-auto ${styles.moduleNav}`}>
                 <li className={`nav-item ${styles.bodyNavActive} ${styles.bodyNavItem}`}>{'Overview'}</li>
                 <li className={`nav-item ${styles.bodyNavItem}`}>{'Photos'}</li>
@@ -50,19 +17,13 @@ const Webpage = props => (
               </ul>
             </nav>
             <div id="overviewModule"></div>
-            <div id="photosModule">
-              <h1>VINCENT'S PHOTO MODULE GOES HERE!</h1>
-            </div>
-            <div id="menusModule">
-              <h1>ANOOP'S MENU MODULE GOES HERE!</h1>
-            </div>
-            <div id="reviewsModule">
-              <h1>CHRYSTAL'S REVIEWS MODULE GOES HERE!</h1>
-            </div>
+            <div id="photo-service"></div>
+            <div id="menusModule"></div>
+            <div id="reviews"></div>
           </div>
         </div>
         <div className="col-3">
-          <div id="reservationModule">
+          <div id="appReserve" className={`sticky-top white`}>
             <h1>ANDREW'S PHOTO MODULE GOES HERE!</h1>
           </div>
         </div>
