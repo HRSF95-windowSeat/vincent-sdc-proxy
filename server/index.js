fef221b4d43d684901593fbfb1d0009aca25666a
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-06-20 10:36:23 -0700

    Formatted proxy to load single component.

--- server/index.js
+++ server/index.js
@@ -7,10 +7,10 @@ const port = process.env.PORT || 8080;
 
 app.use('/restaurant/:restaurantId', express.static(path.join(__dirname, '../public')));
 
-app.use('/overviews', proxy('http://cavatableoverviews-env.5sves92ky9.us-west-1.elasticbeanstalk.com/'));
-app.use('/photos', proxy('http://cavatablephotosv3-env.ispdbjpura.us-west-1.elasticbeanstalk.com/'));
-app.use('/menus', proxy('http://cavatablemenus-env.5sves92ky9.us-west-1.elasticbeanstalk.com/'));
-app.use('/reviews', proxy('http://CavatableFec-env.psexkp69kr.us-west-1.elasticbeanstalk.com/'));
-app.use('/reservations', proxy('http://cavareservations.us-west-2.elasticbeanstalk.com/'));
+// app.use('/overviews', proxy('http://cavatableoverviews-env.5sves92ky9.us-west-1.elasticbeanstalk.com/'));
+// app.use('/photos', proxy('http://cavatablephotosv3-env.ispdbjpura.us-west-1.elasticbeanstalk.com/'));
+// app.use('/menus', proxy('http://cavatablemenus-env.5sves92ky9.us-west-1.elasticbeanstalk.com/'));
+// app.use('/reviews', proxy('http://CavatableFec-env.psexkp69kr.us-west-1.elasticbeanstalk.com/'));
+app.use('/reservations', proxy('http://localhost:3001'));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
